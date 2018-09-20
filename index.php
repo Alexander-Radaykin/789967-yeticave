11acfbb8b6adcd30bcb1e16f1327a889b32d379f
Alexander-Radaykin <a.radaykin@gmail.com>
2018-09-20 18:48:22 +0300

    Добавлена фильтрация данных, полученных от пользователя

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $user_avatar = 'img/user.jpg';
 
 $page_content = include_template('index.php', ['lots_categories' => $lots_categories, 'ads' => $ads]);
 
-$layout_content = include_template('layout.php', ['content' => $page_content, 'lots_categories' => $lots_categories, 'is_auth' => $is_auth, 'title' => 'YetiCave', 'user_name' => $user_name, 'user_avatar' => $user_avatar]);
+$layout_content = include_template('layout.php', ['content' => $page_content, 'lots_categories' => $lots_categories, 'is_auth' => $is_auth, 'title' => 'Главная', 'user_name' => $user_name, 'user_avatar' => $user_avatar]);
 
 print($layout_content);
 ?>
--- templates/index.php
+++ templates/index.php
@@ -17,15 +17,15 @@
         <?php foreach ($ads as $key => $val): ?>
         <li class="lots__item lot">
             <div class="lot__image">
-                <img src="<?=$val['img_link']; ?>" width="350" height="260" alt="<?=$val['name']; ?>">
+                <img src="<?=$val['img_link']; ?>" width="350" height="260" alt="<?=htmlspecialchars($val['name']); ?>">
             </div>
             <div class="lot__info">
                 <span class="lot__category"><?=$lots_categories[$val['category']]; ?></span>
-                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val['name']; ?></a></h3>
+                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($val['name']); ?></a></h3>
                 <div class="lot__state">
                     <div class="lot__rate">
-                        <span class="lot__amount"><?=$val['price']; ?></span>
-                        <span class="lot__cost"><?=cost_format ($val['price']); ?></span>
+                        <span class="lot__amount"><?=htmlspecialchars($val['price']); ?></span>
+                        <span class="lot__cost"><?=cost_format (htmlspecialchars($val['price'])); ?></span>
                     </div>
                     <div class="lot__timer timer">
 
