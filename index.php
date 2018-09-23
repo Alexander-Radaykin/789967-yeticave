c8dcb565b3cbee90b8a97c7af64de54040099acb
Alexander-Radaykin <a.radaykin@gmail.com>
2018-09-23 15:35:45 +0300

    Исправил ошибку с форматом времени

--- index.php
+++ index.php
@@ -11,7 +11,10 @@ $user_avatar = 'img/user.jpg';
 
 $ts_midnight = strtotime('tomorrow');
 $qt_sec_to_midnight = $ts_midnight - time();
-$time_remaining = gmdate('H:i', floor($qt_sec_to_midnight));
+
+$hours = floor($qt_sec_to_midnight / 3600);
+$minutes = floor(($qt_sec_to_midnight % 3600) / 60);
+$time_remaining = sprintf("%02d", $hours) . ' : ' . sprintf("%02d", $minutes);
 
 $content = include_template('index.php', compact('lots_categories', 'ads', 'time_remaining'));
 $layout_content = include_template('layout.php', compact('content', 'lots_categories', 'is_auth', 'title', 'user_name', 'user_avatar'));
