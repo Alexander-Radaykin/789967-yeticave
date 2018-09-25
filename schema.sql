e2ce38e678fc14f5ec9dbcacd49606c78792408d
Alexander-Radaykin <a.radaykin@gmail.com>
2018-09-25 21:21:25 +0300

    Написал код на создание таблицы users

--- schema.sql
+++ schema.sql
@@ -2,3 +2,14 @@ CREATE DATABASE yeticave
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
   
+CREATE TABLE users(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  reg_date DATETIME,
+  email CHAR(60),
+  name CHAR,
+  password CHAR(64),
+  avatar_link CHAR,
+  added_lots_id INT,
+  bets_id INT
+);
+
