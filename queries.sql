8d0f02cfb0931875956f76d2f89179f771dd5932
Alexander-Radaykin <a.radaykin@gmail.com>
2018-09-27 17:59:56 +0300

    Написал запрос на добавление информации в таблицу users

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
+USE yeticave;
+
+-- Запрос на добавление значений в таблицу categories в поле name
 INSERT INTO categories (name) VALUES
 ('Доски и лыжи'),
 ('Крепления'),
@@ -5,3 +8,9 @@ INSERT INTO categories (name) VALUES
 ('Одежда'),
 ('Инструменты'),
 ('Разное');
+
+/* Запрос на добавление значений в таблицу users */
+INSERT INTO users (email, name, password, contacts) VALUES
+('ivan.b@mail.ru', 'Иван', 'birthday', 'Мобильный: +79631156723, Skype: VanoB'),
+('semen.1985@mail.ru', 'Семён', 'mycatsname' 'Мобильный: +79261315541, Skype: Semen85');
+
