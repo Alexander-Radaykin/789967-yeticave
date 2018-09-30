53e555b352a6fac12453cd8cc0732da34feda4b2
Alexander-Radaykin <a.radaykin@gmail.com>
2018-09-30 16:23:48 +0300

    Использовал данные из БД для показа лотов на главной странице

--- templates/index.php
+++ templates/index.php
@@ -14,18 +14,18 @@
         <h2>Открытые лоты</h2>
     </div>
     <ul class="lots__list">
-        <?php foreach ($ads as $key => $val): ?>
+        <?php foreach ($lots as $key => $val): ?>
         <li class="lots__item lot">
             <div class="lot__image">
-                <img src="<?=$val['img_link']; ?>" width="350" height="260" alt="<?=htmlspecialchars($val['name']); ?>">
+                <img src="<?=$val['img_link']; ?>" width="350" height="260" alt="<?=htmlspecialchars($val['title']); ?>">
             </div>
             <div class="lot__info">
                 <span class="lot__category"><?=$val['category']; ?></span>
-                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($val['name']); ?></a></h3>
+                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($val['title']); ?></a></h3>
                 <div class="lot__state">
                     <div class="lot__rate">
                         <span class="lot__amount">Стартовая цена</span>
-                        <span class="lot__cost"><?=cost_format (htmlspecialchars($val['price'])); ?></span>
+                        <span class="lot__cost"><?=cost_format (htmlspecialchars($val['starting_price'])); ?></span>
                     </div>
                     <div class="lot__timer timer">
                     <?=$time_remaining;?>
